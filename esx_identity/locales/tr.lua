3005c850ead861390c725fab7adf1960ad055440
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2020-05-10 03:55:31 +0300

    Create tr.lua

---
+++ esx_identity/locales/tr.lua
@@ -0,0 +1,11 @@
+Locales['tr'] = {
+  ['show_registration'] = 'kayıt Menüsünü göster',
+  ['show_active_character'] = 'aktif Karakteri göster',
+  ['delete_character'] = 'karakterinizi silin ve yeni bir karakter oluşturun',
+  ['deleted_character'] = 'karakter silindi.',
+  ['not_registered'] = 'kayıtlı bir karakteriniz yok.',
+  ['active_character'] = '~b~aktif karakter:~s~ %s %s',
+  ['already_registered'] = 'kayıtlı bir karakteriniz zaten var.',
+  ['failed_identity'] = 'karakterinizi ayarlayamadı, daha sonra tekrar deneyin veya sunucu yöneticisine başvurun!',
+  ['create_a_character'] = 'oyna giriş yapmak için bir karakter oluşturmalısın.'
+}
